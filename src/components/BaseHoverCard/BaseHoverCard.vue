e934fc6ca5def9cffb206166642718c716df81e8
huimintoh <hmintoh@gmail.com>
2019-09-26 18:16:59 +0800

    feat(TheFooter, BaseHoverCard): add skeletal components

---
+++ src/components/BaseHoverCard/BaseHoverCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :id="id" class="card-container">
+    <div
+      class="initialText-container"
+      @mouseover="revealDetailText = true"
+      @mouseout="revealDetailText = false"
+    >
+      <slot name="initialText" />
+    </div>
+
+    <div v-if="revealDetailText" class="detailText-container">
+      <slot name="detailText" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseHoverCard",
+  props() {
+    return {
+      id: {
+        type: String,
+        required: true
+      }
+    };
+  },
+  data() {
+    return {
+      revealDetailText: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card-container {
+  box-sizing: border-box;
+}
+
+.card-container > div {
+}
+
+.initialText-container {
+}
+
+.detailText-container {
+}
+</style>
---
+++ src/components/TheFooter/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <span v-for="(key, index) in Object.keys(office)" :key="index">
+      \
+      <strong>{{ key }}:</strong>
+      {{ office[key] }}
+      \
+    </span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+  data() {
+    return {
+      office: {
+        Address: "#05-03, 196 Pandan Loop, Singapore 128384",
+        Phone: "+65 6779 2226",
+        Emaill: "tim@lambda.sg"
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  text-align: center;
+}
+</style>
