d56f83695d598b0d0639ec9fc095d9cc94ae970c
hmintoh <hmintoh@gmail.com>
2020-03-10 18:09:38 +0800

    refactor(/services): update content

--- src/views/services.vue
+++ src/views/services.vue
@@ -1,115 +1,103 @@
 <template>
   <main>
-    <TheNavbar />
+    <TheNavbar activeRoute="/services" />
 
-    <div class="grid">
-      <section>
-        <BaseHoverCard id="live">
-          <div slot="initialText"><h2 class="heading-h3">LIVE</h2></div>
-          <div slot="detailText">
-            <p>
-              We have been blessed to be a part of many keystone events in some
-              of the largest arenas locally. Attention to detail and proper
-              speaker system design are what we believe allow us to achieve our
-              highest ideals of pursuing presence. Our AV rental partners help
-              us access the best speaker systems from renowned manufacturers
-              like L’Acoustics and d & b Audiotechnik.
-            </p>
-            <p>
-              We have a team of highly experienced audio engineers and
-              consultants who are able to take on any live audio production
-              needs on any scale.
-            </p>
-            <p>Our Key Clients:</p>
-            <ul>
-              <li v-for="(client, index) in liveKeyClients" :key="index">
-                {{ client }}
-              </li>
-            </ul>
+    <div class="main-wrapper">
+      <BaseHoverCard id="live" class="section">
+        <h2 slot="initialText" class="heading-h3">LIVE</h2>
+        <div slot="detailText">
+          <p>
+            We have been blessed to be a part of many keystone events in some of
+            the largest arenas locally. Attention to detail and proper speaker
+            system design are what we believe allow us to achieve our highest
+            ideals of pursuing presence. Our AV rental partners help us access
+            the best speaker systems from renowned manufacturers like
+            L’Acoustics and d & b Audiotechnik.
+          </p>
+          <p>
+            We have a team of highly experienced audio engineers and consultants
+            who are able to take on any live audio production needs at any
+            scale.
+          </p>
+          <p>Our Key Clients:</p>
+          <ul>
+            <li v-for="(client, index) in liveKeyClients" :key="index">
+              {{ client }}
+            </li>
+          </ul>
 
-            <p>Some of our recent Live work include:</p>
-            <ul>
-              <li v-for="(work, index) in liveKeyWorks" :key="index">
-                {{ work }}
-              </li>
-            </ul>
+          <p>Some of our recent Live work include:</p>
+          <ul>
+            <li v-for="(work, index) in liveKeyWorks" :key="index">
+              {{ work }}
+            </li>
+          </ul>
 
-            <p>
-              The full list of clients we have worked for can be found
-              <a href="/portfolio" class="lambda-link">here</a>
-            </p>
-          </div>
-        </BaseHoverCard>
-      </section>
+          <p>
+            The full list of clients we have worked for can be found
+            <a href="/portfolio" class="lambda-link">here</a>
+          </p>
+        </div>
+      </BaseHoverCard>
 
-      <section>
-        <BaseHoverCard id="fixed-installation">
-          <div slot="initialText">
-            <h2 class="heading-h3">FIXED INSTALLATION + CONSULTATION</h2>
-          </div>
-          <div slot="detailText">
-            <p>
-              Our niche has always been local venues especially Houses of
-              Worship with audio design and consultation over the years. Be it
-              full turn-key solutions that fit within the budget required or
-              simply consultation for speaker systems, we are able to support
-              all needs and deliver solutions that far exceed expectations.
-            </p>
-            <p>We have designed for:</p>
-            <ul>
-              <li
-                v-for="(design, index) in fixedInstallationsDesigns"
-                :key="index"
-              >
-                {{ design }}
-              </li>
-            </ul>
-          </div>
-        </BaseHoverCard>
-      </section>
+      <BaseHoverCard id="fixed-installation" class="section">
+        <h2 slot="initialText" class="heading-h3">
+          FIXED INSTALLATION + CONSULTATION
+        </h2>
+        <div slot="detailText">
+          <p>
+            Over the years, our niche has always been local venues especially
+            Houses of Worship with audio design and consultation. Be it full
+            turn-key solutions that respect the budget given or consultation for
+            speaker systems, we are able to support all needs and deliver
+            solutions that far exceed expectations.
+          </p>
+          <p>We have designed for:</p>
+          <ul>
+            <li
+              v-for="(design, index) in fixedInstallationsDesigns"
+              :key="index"
+            >
+              {{ design }}
+            </li>
+          </ul>
+        </div>
+      </BaseHoverCard>
 
-      <section>
-        <BaseHoverCard id="broadcast">
-          <div slot="initialText">
-            <h2 class="heading-h3">BROADCAST + STUDIO</h2>
-          </div>
-          <div slot="detailText">
-            <p>
-              We have worked with various clients on broadcast projects using
-              dedicated broadcast feeds via traditional as well as modern
-              platforms such as Facebook Live.
-            </p>
-            <p>
-              We also have a suite of equipment that allows us to do multitrack
-              live recording and post production for events. Our engineers have
-              vast experience producing the best audio post production that
-              captures the essence and feel of the actual event being
-              documented.
-            </p>
-          </div>
-        </BaseHoverCard>
-      </section>
+      <BaseHoverCard id="broadcast" class="section">
+        <h2 slot="initialText" class="heading-h3">BROADCAST + STUDIO</h2>
+        <div slot="detailText">
+          <p>
+            We have worked with various clients on broadcast projects using
+            dedicated broadcast feeds via traditional as well as modern
+            platforms such as Facebook Live.
+          </p>
+          <p>
+            We also have a suite of equipment that allows us to do multitrack
+            live recording and post production for events. Our engineers have
+            vast experience producing the best audio post production that
+            captures the essence and feel of the actual event being documented.
+          </p>
+        </div>
+      </BaseHoverCard>
 
-      <section>
-        <BaseHoverCard id="acoustic-design">
-          <div slot="initialText">
-            <h2 class="heading-h3">ACOUSTIC DESIGN + TREATMENT</h2>
-          </div>
-          <div slot="detailText">
-            <p>
-              Even the best sound systems can be marred by poor acoustics in a
-              given space. We have dedicated some of our time and energy to help
-              our clients achieve optimal solutions for the acoustics in their
-              given space.
-            </p>
-            <p>
-              We have the ability to advise on acoustic and spatial design while
-              working with architects on new projects as well as improving the
-              acoustics of an existing space.
-            </p>
-          </div>
-        </BaseHoverCard>
-      </section>
+      <BaseHoverCard id="acoustic-design" class="section">
+        <h2 slot="initialText" class="heading-h3">
+          ACOUSTIC DESIGN + TREATMENT
+        </h2>
+        <div slot="detailText">
+          <p>
+            Even the best sound systems can be marred by poor acoustics in a
+            given space. We dedicate our time and energy to help our clients
+            achieve optimal solutions for the acoustics in their given space.
+          </p>
+          <p>
+            We have the ability to advise on acoustic and spatial design while
+            working with architects on new projects as well as improving the
+            acoustics of an existing space.
+          </p>
+        </div>
+      </BaseHoverCard>
     </div>
   </main>
 </template>
@@ -137,14 +125,12 @@ export default {
         "Facebook Singapore"
       ],
       liveKeyWorks: [
-        "Kingdom Invasion 2016 - Suntec City Convention Center",
-        "Who is Jesus Musical - Esplanade Theatres on the Bay",
-        "Kingdom Invasion 2017 - Singapore Expo & Convention Center",
-        "Wan Ren He Yi Song 2017 - Singapore Indoor Stadium",
-        "Snowglobed 2017 - Cornerstone Community Church",
-        "Kingdom Invasion 2018 - Singapore Expo & Convention Center",
-        "The Room Easter Production 2018 - Cornerstone Community Church",
-        "Lighthouse Evangelism 40th Anniversary - Zepp @ Bigbox"
+        "The Tsoh Show Christmas Production – Cornerstone Community Church, Singapore",
+        "Experiencing God The Musical 2019 – St. Andrew’s Cathedral, Singapore",
+        "LifeFest 2019 - St. Andrew’s Cathedral, Singapore",
+        "SingLang 2019 – National Stadium, Singapore Sports Hub",
+        "Asia’s Got Talent Season 3 Grand Finals – Marina Bay Sands Theatre, Singapore",
+        "Asia’s Got Talent Season 3 Judges Auditions & Semifinals – Pinewood Studios, Malaysia"
       ],
       fixedInstallationsDesigns: [
         "St. Andrew's Cathedral Singapore (Cathedral New Sanctuary)",
@@ -171,12 +157,14 @@ main {
   padding: $spacing-xl 0 $spacing-2xl 0;
 }
 
-.grid {
-  > section {
-    width: 100%;
-    border: 1px solid $color-grey-100;
-    margin-bottom: $spacing-m;
-  }
+.section {
+  width: 100%;
+  border: 1px solid $color-grey-100;
+  margin-bottom: $spacing-m;
+}
+
+h2 {
+  margin-bottom: 0;
 }
 
 @media (min-width: $media-query-tablet) {
